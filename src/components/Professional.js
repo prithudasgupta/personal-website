90f9c666ed1188e01b9b45e6bbdc05d1251cdd59
prithudasgupta <prithu_dasgupta@brown.edu>
2020-05-20 01:14:59 -0700

    School content completed

--- src/components/Professional.js
+++ src/components/Professional.js
@@ -7,7 +7,7 @@ export default class Professional extends Component {
         return (
             <div className="Professional">
                 <h1>Professional</h1>
-                <p> I have always been an aspiring software engineer, and here are some of the amazing experiences I have had in the field! </p>
+                <p> I have always been an aspiring software engineer and here are some of the amazing experiences I have had in the field! </p>
                 
                 <ImageBox topic="Facebook: Summer 2020" caption="Because of the COVID-19 Pandemic, my software engineering internship at Facebook for the summer of 2020, which would 
                     have been hosted in Menlo Park, is now remote." imgurl="professional/facebook.png"></ImageBox>
@@ -15,8 +15,8 @@ export default class Professional extends Component {
                 <ImageBox topic="Google: Summer 2019" caption="As an Engineering Practicum intern, I worked on the Shasta team within Google Ads in the Sunnyvale, CA
                     office. My team worked on writing a learning algorithm to reduce cache misses when advertisement requests were queried. " imgurl="professional/google.png"></ImageBox>
 
-                <ImageBox topic="Vistaprint: Summer 2018" caption="After my freshmen year of university, I worked at Vistaprint in Waltham, MA as a software engineering intern. 
-                    I worked on the Hatchery team to create a Facebook messenger bot that customers can use to build customizable phone cases. " imgurl="professional/vistaprint.png"></ImageBox>
+                <ImageBox topic="Vistaprint: Summer 2018" caption="After my freshmen year, I worked at Vistaprint in Waltham, MA as a software engineering intern. 
+                    I worked on the Hatchery team to create a Facebook messenger bot that customers can use to build custom phone cases. " imgurl="professional/vistaprint.png"></ImageBox>
 
                 <ImageBox topic="BU: Summer 2016" caption="Before my senior year of high school, I worked within a Human-Computer Interaction research group at Boston University to 
                     create an adaptable email interface for users with disabilities using the Gmail API." imgurl="professional/bu.png"></ImageBox>
--- src/components/School.js
+++ src/components/School.js
@@ -6,25 +6,53 @@ export default class School extends Component {
         return (
             <div className="School">
                 <h1>School</h1>
-                <p> I am a senior at Brown University studying Computer Science and Applied Mathematics. 
-                    My relevant CS coursework with their updated websites linked are listed here:
-                    <ul>
-                        <li><a href="https://cs.brown.edu/courses/cs019/2017/">Accelerated Introduction to Computer Science</a>: Fall 2017</li>
-                        <li><a href="http://cs.brown.edu/courses/cs032/">Introduction to Software Engineering</a>: Spring 2018</li>
-                        <li><a href="https://cs.brown.edu/courses/csci0220/">Introduction to Discrete Structures and Probability</a>: Spring 2018</li>
-                        <li><a href="http://cs.brown.edu/courses/csci0330/">Introduction to Computer Systems</a>: Fall 2018</li>
-                        <li><a href="https://cs.brown.edu/courses/csci1410/">Artificial Intelligence</a>: Fall 2018</li>
-                        <li><a href="https://cs.brown.edu/courses/csci1420/">Machine Learning</a>: Spring 2019</li>
-                        <li><a href="https://cs.brown.edu/courses/csci1470/">Deep Learning</a>: Fall 2019</li>
-                        <li><a href="https://cs.brown.edu/courses/csci1570/">Design and Analysis of Algorithms</a>: Fall 2019</li>
-                        <li><a href="http://cs.brown.edu/courses/csci1430/">Computer Vision</a>: Spring 2020</li>
-                        <li><a href="http://cs.brown.edu/courses/csci1460/">Computational Linguistics</a>: Spring 2020</li>
-                    </ul>
-                    Student clubs that  I am apart of include Hack@Brown and 180 Degrees Consulting. 
-                    I am lucky enough to have been given the opportunity to TA for classes that have very much shaped my computer 
-                    science education, as I have been a teaching assistant for Introduction to Computer Systems and both a
-                    teaching assistant and head teaching assistant for Introduction to Software Engineering. 
-                </p>                  
+                <p class="school-summary"> I am a rising senior at Brown University studying Computer Science and Applied Mathematics.  
+                    I also have worked as a teaching assistant for Introduction to Computer Systems,
+                    as both a teaching assistant and head teaching assistant for Introduction to Software Engineering, 
+                    and an as organizing lead for Hack@Brown on their sponsorship team. 
+                    My relevant CS coursework with their webpages linked are listed below:</p>
+                    <div class="timeline">
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Fall 2017</h2>
+                        <p><a href="https://cs.brown.edu/courses/cs019/2017/" className="class_links">Accelerated Introduction to Computer Science</a></p>
+                        </div>
+                    </div>
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Spring 2018</h2>
+                        <p><a href="http://cs.brown.edu/courses/cs032/" className="class_links">Introduction to Software Engineering</a></p>
+                        <p><a href="https://cs.brown.edu/courses/csci0220/" className="class_links">Introduction to Discrete Structures and Probability</a></p>
+                        </div>
+                    </div>
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Fall 2018</h2>
+                        <p><a href="http://cs.brown.edu/courses/csci0330/" className="class_links">Introduction to Computer Systems</a></p>
+                        <p><a href="https://cs.brown.edu/courses/csci1410/" className="class_links">Artificial Intelligence</a></p>
+                        </div>
+                    </div>
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Spring 2019</h2>
+                        <p><a href="https://cs.brown.edu/courses/csci1420/" className="class_links">Machine Learning</a></p>
+                        </div>
+                    </div>
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Fall 2019</h2>
+                        <p><a href="https://cs.brown.edu/courses/csci1470/" className="class_links">Deep Learning</a></p>
+                        <p><a href="https://cs.brown.edu/courses/csci1570/" className="class_links">Design and Analysis of Algorithms</a></p>
+                        </div>
+                    </div>
+                    <div class="timeline-container timeline-right">
+                        <div class="timeline-content">
+                        <h2>Spring 2020</h2>
+                        <p><a href="http://cs.brown.edu/courses/csci1430/" className="class_links">Computer Vision</a></p>
+                        <p><a href="http://cs.brown.edu/courses/csci1460/" className="class_links">Computational Linguistics</a></p>
+                        </div>
+                    </div>
+                    </div>    
             </div>
         )
     }
