24698efa9be5db3fda9e886e0132a167cd3856c6
prithudasgupta <prithu_dasgupta@brown.edu>
2020-05-21 20:16:52 -0700

    Updates

---
+++ precache-manifest.1417cfc64bee6e5db5bc87f685d3ef8b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637add770e457beb3a6a71d31c756b8e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "af528db499ec063c1fc3",
+    "url": "/personal-website/static/css/main.af2596fc.chunk.css"
+  },
+  {
+    "revision": "a9016e7e38f914ee5600",
+    "url": "/personal-website/static/js/2.be06221c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.be06221c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af528db499ec063c1fc3",
+    "url": "/personal-website/static/js/main.01b4ccb2.chunk.js"
+  },
+  {
+    "revision": "f50455ddb69baf523794",
+    "url": "/personal-website/static/js/runtime-main.3a9d5e67.js"
+  },
+  {
+    "revision": "4552adfa73b04df512b597ad32be7de0",
+    "url": "/personal-website/static/media/boston-harbor.4552adfa.jpg"
+  },
+  {
+    "revision": "fc114c391ee5b7ec746818c0c4f47467",
+    "url": "/personal-website/static/media/bu.fc114c39.png"
+  },
+  {
+    "revision": "76283ab3f657237f4c3acf38edb55a9d",
+    "url": "/personal-website/static/media/cancun-sunrise.76283ab3.jpg"
+  },
+  {
+    "revision": "ebb2764c7b1159e076e3890a20a95133",
+    "url": "/personal-website/static/media/chapel-bridge.ebb2764c.jpg"
+  },
+  {
+    "revision": "9991cc99f93a058b6dbfe0a3ef5766b1",
+    "url": "/personal-website/static/media/effiel.9991cc99.jpg"
+  },
+  {
+    "revision": "0344dc930ee9601a62cbb447550000fd",
+    "url": "/personal-website/static/media/github.0344dc93.png"
+  },
+  {
+    "revision": "6fcf0327e8d5116bdec0de5830a0341a",
+    "url": "/personal-website/static/media/golden-gate.6fcf0327.jpg"
+  },
+  {
+    "revision": "3d9336ba542645236c6a927c06c5eaf3",
+    "url": "/personal-website/static/media/google.3d9336ba.png"
+  },
+  {
+    "revision": "885c69c8d0f1070e9c081ef0c8582834",
+    "url": "/personal-website/static/media/jungfrau.885c69c8.jpg"
+  },
+  {
+    "revision": "73c798261d1d9f6907b13d7ade19fa16",
+    "url": "/personal-website/static/media/lake-lucerne.73c79826.jpg"
+  },
+  {
+    "revision": "64f86a63d29fc316f60e4319a6549aab",
+    "url": "/personal-website/static/media/license.64f86a63.png"
+  },
+  {
+    "revision": "3cea36a2225ed2d71858c8b7e28adf8d",
+    "url": "/personal-website/static/media/light.3cea36a2.png"
+  },
+  {
+    "revision": "7a8d9866d8b00bb3599a478699ab5086",
+    "url": "/personal-website/static/media/montreux-sunset.7a8d9866.jpg"
+  },
+  {
+    "revision": "c940a56e742502bc8eb7e9a747342c5f",
+    "url": "/personal-website/static/media/music.c940a56e.png"
+  },
+  {
+    "revision": "fd5c1dad928726c96a01870e0b32d819",
+    "url": "/personal-website/static/media/standing-glaciers.fd5c1dad.jpg"
+  },
+  {
+    "revision": "53519aa516735b2f0a6d2dc9df4e933f",
+    "url": "/personal-website/static/media/swiss-train.53519aa5.jpg"
+  },
+  {
+    "revision": "2e113d5638e5e26ea98fe5319ccb471f",
+    "url": "/personal-website/static/media/tanks.2e113d56.png"
+  },
+  {
+    "revision": "c54b70defd37713112c80571486f5473",
+    "url": "/personal-website/static/media/trevi-fountain.c54b70de.jpg"
+  },
+  {
+    "revision": "72e92d86d9ae22bf3ccd915d4e26835b",
+    "url": "/personal-website/static/media/venice-cruise.72e92d86.jpg"
+  },
+  {
+    "revision": "0223e0ded35ed607aa9e3ce68f33f312",
+    "url": "/personal-website/static/media/vienna.0223e0de.jpg"
+  },
+  {
+    "revision": "2cd1337676495d27410b04befa6850eb",
+    "url": "/personal-website/static/media/vistaprint.2cd13376.png"
+  }
+]);
